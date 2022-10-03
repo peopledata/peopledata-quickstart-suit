843cc63f22648c199072faef375a42666e6ca026
jerry zhang <79302978+0x1bdat@users.noreply.github.com>
2022-10-03 20:30:21 +0800

    Update master_init.sh

--- k8s/master_init.sh
+++ k8s/master_init.sh
@@ -4,12 +4,4 @@ kubeadm config images list --image-repository registry.aliyuncs.com/google_conta
 # 获取镜像文件至本地
 kubeadm config images pull --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.25.2
 
-kubeadm init \
---image-repository registry.aliyuncs.com/google_containers \
---control-plane-endpoint apiserver.host.com \
---kubernetes-version v1.18.1 \
---apiserver-advertise-address 10.7.79.148 \
---service-cidr 192.168.0.0/16 \
---pod-network-cidr 172.16.0.0/16 \
---token-ttl 0 \
---upload-certs
+kubeadm init --image-repository registry.aliyuncs.com/google_containers 
