5f3c10813d60fda98f4f99bda487f5eb6dc108d8
jerry zhang <79302978+0x1bdat@users.noreply.github.com>
2022-10-04 12:01:57 +0800

    Update quick_install.sh

--- k8s/quick_install.sh
+++ k8s/quick_install.sh
@@ -1,3 +1,98 @@
+#!/bin/bash
+# jerry.zhang
+
+echo "***************************** quick install ****************************"
+echo "this quick install will install basic software:"
+echo " 1. docker"
+echo " 2. nodejs"
+echo " 3. kubectl"
+echo " 4. minikube"
+
+echo "************************************************************************"
+
+read -n1 -p " Do you want to  continue [y/n]" answer
+
+case $answer in 
+	(Y | y )
+		echo "fine, let's start install";;
+	(N | n)
+		exit 0;;
+esac
+
+
+echo "********************** install docker *********************"
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-compose-plugin=2.3.3~ubuntu-focal
+
+
+echo "Check if docker installed, you could run
+
+$ sudo docker run hello-world
+
+"
+
+# let docker run without root privaledge
+
+sudo usermod -aG docker $USER
+
+# install docker-compse
+
+sudo apt-get update
+sudo apt-get install docker-compose
+
+sudo apt-get install docker-compose-plugin=
+
+
+echo "*********************** install nodejs *********************"
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install nodejs
+node --version
+
+echo "nodejs installed!"
+
+
+echo "******************** install kubeadm/kubelet/kubectl ******************"
+
+
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+echo "minikube installed!" 
+echo "reboot your machine, then start work!"
+
+# install helm using snap
+
+sudo snap install helm --classic
+
+sudo apt install jq
+
+
+
+
+#echo "******************** install kubeadm kubelet **************”
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+
 
 
 #echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -7,7 +102,7 @@ sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-k
 sudo add-apt-repository "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main"
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm
-sudo apt-mark hold kubelet kubeadm
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo swapoff -a
